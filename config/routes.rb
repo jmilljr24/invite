96744065f1dd37b014444ce030a4cad568a25a38
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-04-02 15:56:06 -0400

    Add admin and dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard/' => "dashboard#index", as: :dashboard
+  devise_for :admins
+  resources :invitations
   devise_for :users
   get 'pages/index'
   get 'page/index'
@@ -7,7 +10,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   root "pages#index"
 end
