c3c2a0ce18405694bb637c641011904c617eba83
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-04-02 18:07:39 -0400

    Add admin under devise user. Only admin can create account

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'dashboard/' => "dashboard#index", as: :dashboard
-  devise_for :admins
+  get "dashboard/" => "dashboard#index", :as => :dashboard
   resources :invitations
-  devise_for :users
-  get 'pages/index'
-  get 'page/index'
+
+  devise_for :users, controllers: {registrations: "users/registrations"}
+  # resources :users, except: :create
+  # post "create_user" => "users/registrations#create", :as => :create_user
+
+  get "pages/index"
+  get "page/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
   # Defines the root path route ("/")
   root "pages#index"
 end
