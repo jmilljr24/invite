f944e14192a10d409326bc45711ae325defbffa9
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-04-04 18:18:19 -0400

    Add guest login with turbo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
   # before_action :authenticate_user!
+  before_action :set_profile, only: [:login]
   def index
+  end
+
+  def login
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:profile_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   resources :profiles
-  get "dashboard/" => "dashboard#index", :as => :dashboard
 
+  get "dashboard/" => "dashboard#index", :as => :dashboard
+  get "guest/login" => "pages#login", :as => :guest_login
   devise_for :users, controllers: {registrations: "users/registrations"}
 
   # resources :users, except: :create
   # post "create_user" => "users/registrations#create", :as => :create_user
 
-  get "pages/index"
-  get "page/index"
+  # get "pages/index"
+  # get "page/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
